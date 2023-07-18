8b0a20e949507103217bdcff33b7adeeba53a381
Seth Berke <sethberke@gmail.com>
2023-07-18 14:44:25 -0400

    Dynamic Functionality checked

--- chunk_large_vcf.py
+++ chunk_large_vcf.py
@@ -20,6 +20,7 @@ def split_file(file_path):
                 break
         
         num_header_lines = len(header_lines)
+        print("NUM HEADERLINES: ", num_header_lines)
 
         if total_lines <= num_header_lines + output_file_size:
             # If the total lines are smaller or equal to the output file size
@@ -35,18 +36,23 @@ def split_file(file_path):
             return
 
         total_parts = math.ceil((total_lines - num_header_lines) / output_file_size)
-        lines_per_part = math.ceil((total_lines - num_header_lines) / total_parts)
+        lines_per_part = (total_lines - num_header_lines) // total_parts
+        remainder_lines = (total_lines - num_header_lines) % total_parts
 
+        line_index = num_header_lines
         for part in range(total_parts):
-            start_index = num_header_lines + (part * lines_per_part)
-            end_index = min(start_index + lines_per_part, total_lines)
             output_file = f"RUNID_part_{part+1}_of_{total_parts}.txt"
+            lines_in_part = lines_per_part
+            if part < remainder_lines:
+                lines_in_part += 1
+            end_index = line_index + lines_in_part
 
             with open(output_file, 'w') as output:
                 output.writelines(header_lines)  # Write header lines
-                output.writelines(lines[start_index:end_index])
+                output.writelines(lines[line_index:end_index])
 
-            print(f"Part {part+1} written to {output_file} with {end_index - start_index} lines.")
+            print(f"Part {part+1} written to {output_file} with {lines_in_part} lines.")
+            line_index = end_index
 
         print("Total lines:", total_lines)
 
